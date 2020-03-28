2308515621545ff5fbe1d1ac4e6b9eac84131724
batandos97 <batandos158@gmail.com>
2020-03-28 13:25:13 +0700

    File tes.php used for create unfixed program

---
+++ tes.php
@@ -0,0 +1,18 @@
+<?php
+$domain_extension = array("");
+$url = explode(".", parse_url("http://www.kemenparekraf.go.id/")["host"]);
+
+print_r($url);
+$new_str = "www.kemenparekraf";
+$c = ".go.id";
+// if (preg_match("/{$c}/i", $a, $m)) {
+//     echo var_dump($m);
+// }
+$ar = array(".com");
+$new_str = preg_replace(array("/\A(www.)+/","/(\.(go|sch|edu|org|com|id))*\z/"), '', $new_str);
+echo $new_str."\n";
+
+// $new_str = preg_replace("/\A(www.)/", '', $new_str);
+// echo $new_str."\n";
+
+?>
